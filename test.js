310aee3c881ef0c7e5c8aae912f8cf70558a3abc
KongJinYong <dimeon@naver.com>
2021-09-27 15:41:50 +0900

    typescript 연습

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
-// function plus(a, b) {
+// function plus(a:number, b) {
 //     return a + b;
 // }
-
-// console.log(plus("la", 2));
+var plus = function (a, b) { return a + b; };
+var hello = "hello";
+console.log(plus(1, 2));
+var info = function (name, age) { return "my name is " + name + " , i'm " + age + " years old!"; };
+console.log(info("dota", 19));
+var jinyong = {
+    age: 18,
+    name: "jin",
+    feel: "good"
+};
+//const humnaInfo = (human) => `my name is ${human.name} i feel ${human.feel} , ${human.age} old!`;
+//console.log(humnaInfo(jinyong));
--- test.ts
+++ test.ts
@@ -1,5 +1,23 @@
-function plus(a, b) {
-    return a + b;
+// function plus(a:number, b) {
+//     return a + b;
+// }
+
+const plus = (a:number,b:number) => a+b;
+
+let hello:string = "hello";
+
+console.log(plus(1, 2));
+
+const info = (name:string, age:number):string => `my name is ${name} , i'm ${age} years old!`
+
+console.log(info("dota",19));
+
+const jinyong = {
+    age: 18,
+    name: "jin",
+    feel: "good"
 }
 
-console.log(plus("la", 2));
+//const humnaInfo = (human) => `my name is ${human.name} i feel ${human.feel} , ${human.age} old!`;
+
+//console.log(humnaInfo(jinyong));
